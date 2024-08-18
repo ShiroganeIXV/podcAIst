44630a75d933c6034d5ebd5d2ede4419516d7d41
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-08-18 12:43:12 +0700

    ADD main & rightSidebar section outline

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,8 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
 import type { Metadata } from "next";
+import Image from "next/image";
 // import { Inter } from "next/font/google";     //? b/c already imported in main layout.tsx
 // import "./globals.css";
 
@@ -10,13 +13,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>
-            {/* <p className="text-white-1">Left sidebar</p> */}
+    <div className="relative flex flex-col">
+        <main className="relative flex bg-black-3">
+            {/* //TODO <p className="text-white-1">Left sidebar</p> */}
             <LeftSidebar />
-            {children} {/* main */}
+
+            <section className="border-2 border-red-500 min-h-screen flex-1 flex-col px-4 sm:px-14">
+              <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+                <div className="flex h-16 items-center justify-between md:hidden">
+                  <Image src={"/icons/podcAIst-logo.svg"} alt="menu icon" width={40} height={40}/>
+                  {/*//TODO MobileNav */}
+                  <MobileNav />
+                </div>
+                <div className="flex flex-col md:pb-14">
+                  Toaster Nofification (Shadcn)
+                  {children} {/*//TODO main */}
+                </div>
+              </div>
+            </section>
+
+
             
-            <p className="text-white-1">Right sidebar</p>
+            
+            {/* //TODO <p className="text-white-1">Right sidebar</p> */}
+            <RightSidebar />
         </main>
     </div>
   );
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <section className="right_sidebar text text-white-1">
+      RightSidebar
+    </section>
+
+  )
+}
+
+export default RightSidebar
